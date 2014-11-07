13018e5dd4077eaa411bb658c3fe2081d8046cd1
mayi <mayi@live.com>
2014-11-07 18:18:24 +0800

    add preparenotes

--- server/service.py
+++ server/service.py
@@ -6,18 +6,19 @@ from evernote.api.client import EvernoteClient
 class NoteService():
     def __init__(self, dev_token):
     	if dev_token is not None:
-            self.dev_token = dev_token
-            self.client = EvernoteClient(token=self.dev_token)
+            self._dev_token = dev_token
+            self._client = EvernoteClient(token=self._dev_token)
+            self._note_names = ["1", "2", "3", "4"]
         else:
             raise Error
 
     def connect(self):
-        userStore = self.client.get_user_store()
+        userStore = self._client.get_user_store()
         user = userStore.getUser()
         return user.username
 
     def prepareNotebook(self):
-        note_store = self.client.get_note_store()
+        note_store = self._client.get_note_store()
         notebooks = note_store.listNotebooks()
         l = []
         for n in notebooks:
@@ -29,13 +30,18 @@ class NoteService():
         return notebook.guid
 
     def prepareNotes(self, notebookGuid):
-        note_filter = NTypes.NoteFilter(notebookGuid=notebookGuid)
+        note_store = self._client.get_note_store()
+
+        note_filter = NTypes.NoteFilter()
+        note_filter.notebookGuid = notebookGuid
         result_spec = NTypes.NotesMetadataResultSpec(includeTitle=True, includeNotebookGuid=True)
-        note_store = self.client.get_note_store()
         meta_list = note_store.findNotesMetadata(note_filter, 0, 4, result_spec)
-        return meta_list.totalNotes()
+
+        for meta in meta_list:
+
+        return meta_list.totalNotes
 
 if __name__ == '__main__':
     dev_token = "S=s1:U=8fcf8:E=150da644ca6:C=14982b31da8:P=1cd:A=en-devtoken:V=2:H=a8defc28f091744b9ebfaea80f5c1d58"
     ns = NoteService(dev_token)
-    print ns.prepareNotes(ns.prepareNotebook)
+    print ns.prepareNotes(ns.prepareNotebook())

9cdf8af24829509cd388eadcae20769732e5bce9
mayi <mayi@live.com>
2014-11-11 19:16:50 +0800

    add load_all_note function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 if (typeof jQuery === 'undefined') { throw new Error('requires jQuery') }
 
 $(document).ready(function() {
-	
+
+}
+
+function load_all_note() {
+	$.getJSON("/loadall", function(data) {
+		
+	});
 }

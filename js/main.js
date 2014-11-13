5451dabc2a94f736794b38a64f2f88b7a0947f03
mayi <mayi@live.com>
2014-11-13 19:26:18 +0800

    buget

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
 function load_all_note() {
 	$.getJSON("/i/loadall", function(data) {
 		$.each(data, function(key, value) {
+			var nc = value.length;
 			var array = [];
 			for (i = 0; i < value.length; i++) {
 				var v = value[i];
@@ -14,8 +15,10 @@ function load_all_note() {
 				array.push('<input type="checkbox"' + (v.c == 1 ? "checked" : "") + '/>');
 				array.push(v.t);
 				array.push('</p>');
+				nc -= v.c;
 			}
 			$("#" + key + "-div").html(array.join(''));
+			$("#" + key + "-div").parent().find('.badge').html(nc + "");
 		});
 	});
 }
